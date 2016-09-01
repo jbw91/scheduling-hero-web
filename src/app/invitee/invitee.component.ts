02fa3898916c868026a3265b0416ce39badb3fec
Gavin Nitta <gshigeto@gmail.com>
2016-09-01 12:52:51 -0600

    Create admin, invitee, and home page. Get materialize working

---
+++ src/app/invitee/invitee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invitee',
+  templateUrl: './invitee.component.html',
+  styleUrls: ['./invitee.component.css']
+})
+export class InviteeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
