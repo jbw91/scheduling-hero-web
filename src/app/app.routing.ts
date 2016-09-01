2de87ad64ac96b22fc925369ba3f12a32081972b
Gavin Nitta <gshigeto@Gavins-MacBook-Pro.local>
2016-09-01 12:52:51 -0600

    Create admin, invitee, and home page. Get materialize working

---
+++ src/app/app.routing.ts
@@ -0,0 +1,19 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { InviteeComponent } from './invitee/invitee.component';
+import { AdminComponent } from './admin/admin.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'invitee', component: InviteeComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: '**', component: HomeComponent }
+];
+
+export const appRoutingProviders: any[] = [
+
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
