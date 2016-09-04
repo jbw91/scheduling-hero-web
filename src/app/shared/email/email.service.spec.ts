a2d28dc04aa9be484cb1f3e040c9537c0e62a284
John Woodruff <johnwoodruff91@gmail.com>
2016-09-04 00:46:32 -0600

    Fix Email service test

--- src/app/shared/email/email.service.spec.ts
+++ src/app/shared/email/email.service.spec.ts
@@ -2,11 +2,12 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { EmailService } from './email.service';
+import { HTTP_PROVIDERS } from '@angular/http';
 
 describe('Service: EmailService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [EmailService]
+      providers: [EmailService, HTTP_PROVIDERS]
     });
   });
 
