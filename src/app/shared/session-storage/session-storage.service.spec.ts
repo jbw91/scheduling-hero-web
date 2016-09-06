94b822611e3dbe217afe506e5317f80ec8180ea0
Nitta <gshigeto@gmail.com>
2016-09-06 14:45:31 -0600

    Add tests to session storage

--- src/app/shared/session-storage/session-storage.service.spec.ts
+++ src/app/shared/session-storage/session-storage.service.spec.ts
@@ -10,7 +10,16 @@ describe('Service: SessionStorage', () => {
     });
   });
 
-  it('should ...', inject([SessionStorageService], (service: SessionStorageService) => {
-    expect(service).toBeTruthy();
+  it('should set session storage variables correctly', inject([SessionStorageService], (service: SessionStorageService) => {
+    spyOn(service, 'setToken');
+    service.setToken('test', 'this is a test');
+    expect(service.setToken).toHaveBeenCalledWith('test', 'this is a test');
+  }));
+
+  it('should get session storage variables correctly', inject([SessionStorageService], (service: SessionStorageService) => {
+    service.setToken('test', 'this is a test');
+    return service.getToken('test').toPromise().then(result => {
+      expect(result).toBe('this is a test');
+    })
   }));
 });
